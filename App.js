c59611865b0f8b1286392073025c4c61c6b5a0fb
Prashant Verma <prashant@7isys.com>
2018-12-20 15:18:22 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>ur appdsfdsfakjdfalkflkahf
+		akjlkasjlkjk
+		asdkasjdkj</Text>
+		 <Text>ur appdsfdsfakjdfalkflkahf
+		akjlkasjlkjk
+		asdkasjdkj</Text>
+		 <Text>ur appdsfdsfakjdfalkflkahf
+		akjlkasjlkjk
+		asdkasjdkj</Text>
+		 <Text>ur appdsfdsfakjdfalkflkahf
+		akjlkasjlkjk
+		asdkasjdkj</Text>
+		 <Text>ur appdsfdsfakjdfalkflkahf
+		akjlkasjlkjk
+		asdkasjdkj</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
